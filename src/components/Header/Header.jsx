dabb48957ebac690314a644a202e2884724a4fcd
joy4563 <joymondal4563@gmail.com>
2023-04-01 00:25:48 +0600

    Header Done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-style'>
+                <p>Knowledge Cafe</p>
+                <img src="pers.png" alt="" />
+              
+            
+        </div>
+    );
+};
+
+export default Header;
